513cc24615aed07273ba85864486d19e4ba9f37d
László Kovács <68534070+KoLa992@users.noreply.github.com>
2022-10-03 11:10:30 +0200

    Add files via upload

---
+++ ReSampling.R
@@ -0,0 +1,16 @@
+DrawSamples <- function(current_data, sample_size, number_of_samples, random_seed) {
+  set.seed(random_seed)
+  Samples <- sample(Emp$Income, size = sample_size, replace = TRUE, )
+  
+  for (i in 2:number_of_samples) {
+    set.seed(random_seed+i-1)
+    Samples <- rbind(Samples, sample(Emp$Income, size = sample_size, replace = TRUE, ))
+  }
+  
+  Samples <- as.data.frame(Samples)
+  
+  rownames(Samples) <- paste0("Sample",1:number_of_samples)
+  colnames(Samples) <- paste0("Element",1:sample_size)
+  
+  return(Samples)
+}
